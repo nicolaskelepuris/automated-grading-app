5f149f73c6292d418c4771da9878f3a5895c91a4
vinicius <priori987@gmail.com>
2022-08-28 18:59:24 -0300

    Cleanup, icons and centralization

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG
+
+## [0.0.0] - 2022-08-27
+### ADDED
+    - React base
+    - Uploading files and sending requests via FormData with multipart
+    - Host application on AWS
+
+ ## [0.0.1] - 2022-08-27
+### ADDED
+    - Changed favicon.ico
+    
--- src/FileUploadForm.jsx
+++ src/FileUploadForm.jsx
@@ -4,6 +4,7 @@ class FileUploadForm extends React.Component {
     constructor(props) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
+    
     }
 
     async handleSubmit(event) {
@@ -14,17 +15,26 @@ class FileUploadForm extends React.Component {
 
     createRequest(form) {
         const body = new FormData(form);
-        return new Request('https://automated-grading-api.herokuapp.com/upload-file', { method: 'POST', body, header: { 'Accept': 'application/json' } })
+        return new Request('https://automated-grading-api.herokuapp.com/', { method: 'POST', body, header: { 'Accept': 'application/json' } })
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor="files">Select files:</label>
-                <input type="file" className='form-control' id="files" name="files" multiple accept="image/jpg, image/jpeg, image/png" />
+            <div className='FileUploadForm' style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+                <h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                         <label htmlFor="files" > Select files:</label>
+                        </div> 
+                        <input type="file" className='form-control' id="files" name="files" multiple accept="image/jpg, image/jpeg, image/png" />
+                         <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}> 
+                        <input type="submit" className='btn btn-primary mt-2' value="Enviar"/>
+                        </div>
+                        <label htmlFor="files"></label>
 
-                <input type="submit" className='btn btn-primary mt-2' value="Enviar" />
-            </form>
+                    </form>
+                </h1>
+                </div>
         );
     }
 }
